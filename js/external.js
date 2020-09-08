95840a4243a2bda1d7c8e4cadbd2b06d39e9319d
Nguyenkien849 <69201670+Nguyenkien849@users.noreply.github.com>
2020-09-08 15:02:29 +0700

    Development (#13)
    * test
    
    * Responsive (#2)
    
    * Feature/responsive (#3)
    
    * Responsive
    
    * Responsive again
    
    * test
    
    * Feature/responsive (#4)
    
    * Responsive
    
    * Responsive again
    
    * Responsive
    
    * Feature/responsive (#5)
    
    * Responsive
    
    * Responsive again
    
    * Responsive
    
    * Add Jquery (#6)
    
    * Feature/fixpage (#7)
    
    * Add Jquery
    
    * Change partner
    
    * Merge
    
    * Feature/fixpage (#8)
    
    * Add Jquery
    
    * Change partner
    
    * Merge
    
    * Change js (#10)
    
    * Fix (#11)
    
    * fix (#12)

---
+++ js/external.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+    $('.comebacktop').click(function (e) { 
+        e.preventDefault();
+        $('html,body').animate({ scrollTop: 0}, 1500, "easeInOutSine");
+    });
+   
+    
+    // Menu ẩn khi chưa click
+    $('.menu-mobile').animate({opacity: 0, marginLeft:-300});
+
+     // Menu hiện ra và thu lại khi click
+
+    $('.nav-mobile').click(function() {
+        $('.menu-mobile').animate({opacity: 0, marginLeft:-300});
+        $('.menu-mobile').toggleClass('menu-mobile-1');
+        $('.menu-mobile-1').animate({opacity: 1, marginLeft:0}, 800, "easeInOutSine");
+    });
+    
+
+    $('.search-box').animate({opacity: 0, marginTop:-50});
+
+    $('.search').click(function() {
+        $('.search-box').animate({opacity: 0, marginTop:-50});
+        $('.search-box').toggleClass('search-box-1');
+        $('.search-box-1').animate({opacity: 1, marginTop:0}, 500);
+    });
+
+    $('.recieve').click(function() {
+        $('html,body').animate({scrollTop: $('.form').offset().top}, 1000);
+    });
+});
+
+
+
+
